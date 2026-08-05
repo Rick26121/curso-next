b2040249267c2484d715c683488dba06e77d3b69
ricardo <ricardo26121@gmail.com>
2026-08-04 21:15:41 -0400

    cambios estaba cansado sigo mañana

--- 02-mydashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-mydashboard/src/app/dashboard/pokemons/page.tsx
@@ -20,17 +20,10 @@ export default async function PokemonsPage() {
 
   return (
     <div className='flex flex-col'>
-    <div className="flex flex-wrap gap-10 items-center justify-center">
-     {pokemons.map((pokemon) => (
-       <Image
-       key={pokemon.id}
-       src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-       width={100}
-       height={100}
-       alt={pokemon.name}
-       />
-     ))}
-    </div>
+      <span className='text-5xl my-2'> Listado de pokemons <small>Estático</small></span>
+
+
+    
     </div>
   );
 
